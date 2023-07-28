32d77fd42899d131a5f5294511d9e2852d0523d3
Luis Enrique <luiskike2210@gmail.com>
2023-07-28 16:18:48 -0600

    07 insert and select visitas

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, session, redirect, url_for, Blueprint, flash
+from flask import Flask, render_template, request, session, redirect, url_for, Blueprint, flash, jsonify
 import mysql.connector
+import os 
 from models.db import get_connection
 usuarios_registrados = []
 visitantes_registrados = []
 items_registrados = []
+visitas=[]
 
 
 app = Flask(__name__, template_folder='inicio/templates' , static_folder='inicio/static' )
@@ -18,6 +20,7 @@ admin_bp = Blueprint('admin', __name__, template_folder='administrador/templates
 ayudante_bp = Blueprint('ayudante', __name__, template_folder='ayudante/templates')
 app.register_blueprint(admin_bp, url_prefix='/admin')
 app.register_blueprint(ayudante_bp, url_prefix='/ayudante')
+app.secret_key = os.urandom(24)
 
 @app.route('/inicio', endpoint='inicio.index')
 def inicio():
@@ -70,10 +73,27 @@ def registro():
         return render_template('registro.html')
     
 
+def verificar_credenciales(username, password):
+    try:
+        mydb = get_connection()
+        cursor = mydb.cursor()
+        sql = "SELECT * FROM usuario WHERE nomUsuario = %s"
+        cursor.execute(sql, (username,))
+        user_data = cursor.fetchone()
+        cursor.close()
+        mydb.close()
 
+        if user_data and user_data['contrasenia'] == password:
+            return True
+        return False
+
+    except Exception as e:
+        print("Error al verificar las credenciales:", str(e))
+        return False
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+       if request.method == 'POST':
         tipoCargo = request.form['tipoCargo']
         if tipoCargo == 'Administrador':
             # Redireccionar al usuario a la sesión de admin
@@ -84,7 +104,7 @@ def login():
         flash('Tipo de usuario no válido. Por favor, verifica tus datos' , 'error')
         return redirect(url_for('login'))  # Redirigir al formulario de inicio de sesión nuevamente.
 
-    else:
+       else:
         # Renderizar el formulario de inicio de sesión si es una solicitud GET
         return render_template('login.html')
 
@@ -141,7 +161,6 @@ def registrarItem():
         # Mostrar el formulario de registro
         return render_template('registrarItem.html')
     
-
 @app.route('/item')
 def item():
         try: 
@@ -214,11 +233,66 @@ def visitante():
          mydb = get_connection()
          cursor = mydb.cursor(dictionary=True)
          cursor = mydb.cursor(dictionary=True)
-         cursor.execute('SELECT nombre, ape_pat, ape_Mat, Origen, edad, genero, institucion, fecha_registro FROM visitante')
+         cursor.execute('SELECT id_visitante, nombre, ape_pat, ape_Mat, Origen, edad, genero, institucion, fecha_registro FROM visitante')
          visitantes = cursor.fetchall()
          cursor.close()
          return render_template('visitante.html', visitantes=visitantes)
     # Renderiza la plantilla HTML con los resultados
+
+@app.route("/registroVisitas", methods= ['GET','POST'])
+def registroVisitas():
+        # Obtener los datos enviados desde el formulario
+
+    if request.method == 'POST':
+        # Obtener los datos enviados desde el formulario
+         id_visitante = request.form['id_visitante']
+         cantidad_visitante = request.form['cantidad_visitante']
+         fecha_visita = request.form['fecha_visita']
+         tipo_visita = request.form['tipo_visita']
+        
+
+        
+            # Obtener la conexión a la base de datos
+         mydb = get_connection()
+         cursor = mydb.cursor()
+
+            # Consulta SQL para insertar los datos en la tabla de usuarios
+         sql = "INSERT INTO visitas (id_visitante, cantidad_visitante, fecha_visita, tipo_visita) VALUES (%s, %s, %s, %s)"
+         val = (id_visitante, cantidad_visitante, fecha_visita, tipo_visita)
+
+            # Ejecutar la consulta SQL
+         cursor.execute(sql, val)
+
+            # Confirmar los cambios en la base de datos
+         mydb.commit()
+
+            # Cerrar el cursor y la conexión a la base de datos
+         cursor.close()
+         mydb.close()
+
+            # Redireccionar a la página de inicio después de registrar al usuario
+         return redirect(url_for('registroVisitas'))
+
+    else:
+        # Mostrar el formulario de registro
+        return render_template('registroVisitas.html')
+
+@app.route("/visitas")
+def visitas():
+    try:        
+        mydb = get_connection()
+        cursor = mydb.cursor(dictionary=True)
+        cursor = mydb.cursor(dictionary=True)
+        cursor.execute('SELECT vs.id_visita AS "ID", CONCAT (v.ape_pat," ", v.ape_Mat," ", v.nombre) AS "Nombre Completo", vs.cantidad_visitante AS "Numero de visitantes", vs.fecha_visita AS "Fecha de Visita", vs.tipo_visita AS "Tipo de visita" FROM visitas AS vs INNER JOIN visitante AS v ON vs.id_visitante = v.id_visitante ')
+        visitas = cursor.fetchall()
+        cursor.close()
+        return render_template('visitas.html', visitas=visitas)
+    except mysql.connector.Error as err:
+        print ("error en la consulta:", err)
+
+@app.route("/editarVisitante")
+def editarVisitante():
+    return render_template('editarVisitante.html')
 
 @app.route('/usuario')
 def usuario():
